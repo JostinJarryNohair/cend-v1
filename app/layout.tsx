319ea8ae18aa67ddc46ea5c46be07b958747467b
JostinJarryNohair <jostin@luxevotech.com>
2025-02-07 21:55:34 -0500

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
-
+import type { Metadata } from "next";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-}
+  title: "v0 App",
+  description: "Created with v0",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
-  )
+  );
 }
